a8cbed050c8d8ca05f3bb392952a6fc3d52eb81f
Cimorexave <sadeq.s502@gmail.com>
2022-06-17 12:59:20 +0430

    added app.use to send assets styling; fixed homepage

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ console.log('*** started app.js ***')
 //import * as homePage from './public/index.html';
 const express = require("express")
 const app = express()
+const path = require("path")
 
 app.listen('5000', () => {
     console.log('server listeing on port 5000...')
 })
 
+app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
     console.log('home page')
-    res.status(200).send("./public/index.html")
-    res.sendFile("./public/style.css")
+    res.status(200).send(path.resolve(__dirname, './public/index.html'))
+    //res.sendFile("./public/style.css")
 }) 
 app.get('/about', (req, res) => {
     res.status(200).send("<h1>About Page</h1>")
