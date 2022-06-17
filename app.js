74db888d532c79bf8b77bbca6786aba5f37b228f
Cimorexave <sadeq.s502@gmail.com>
2022-06-17 12:54:34 +0430

    server made and paths were created

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log('*** started app.js ***')
+//const homePage = require("./public/index.html")
+//import * as homePage from './public/index.html';
+const express = require("express")
+const app = express()
+
+app.listen('5000', () => {
+    console.log('server listeing on port 5000...')
+})
+
+
+app.get('/', (req, res) => {
+    console.log('home page')
+    res.status(200).send("./public/index.html")
+    res.sendFile("./public/style.css")
+}) 
+app.get('/about', (req, res) => {
+    res.status(200).send("<h1>About Page</h1>")
+})
